192b46ba31eecea62f821a2d6fed4f18504e2180
Gabriel Leon <gabriel.leonaveleyra@tapinfluence.com>
2016-08-08 22:52:03 -0600

    a tutorial that explains for loops in Java as well as two ways to print to the console

---
+++ src/Application.java
@@ -0,0 +1,11 @@
+
+public class Application {// here we will look at for loops!
+
+	public static void main(String[] args) {
+		for (int i = 0; i <= 10; i++) {// this loop will count from 0-10 syntax is very similar to JS!
+			System.out.println("The value of i is now " + i);// the \n character prints a new line!
+			System.out.printf("The value of i is now %d \n", i);// this is like println but as a format specifyer, i.e. replace all %something with somethingElse
+		}
+	}
+
+}
